c47c646f71557bd5951492362f7df85869ccfdcc
Leonel San Martin <31077603+leo9210@users.noreply.github.com>
2021-10-27 17:26:03 -0300

    Comienzo de activity de patron de desbloqueo

--- app/src/main/java/com/example/recuperarte/MainActivity.java
+++ app/src/main/java/com/example/recuperarte/MainActivity.java
@@ -4,7 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.andrognito.patternlockview.PatternLockView;
+
 public class MainActivity extends AppCompatActivity {
+
+    String savePatternKey = "patternCode";
+    String finalPattern = "";
+    PatternLockView mPatternLockView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
