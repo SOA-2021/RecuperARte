f406a890687f2a8aa109bb4a35e90fa958681678
Leonel San Martin <31077603+leo9210@users.noreply.github.com>
2021-10-28 00:09:48 -0300

    Creacion de SignUp

--- app/src/main/java/com/example/recuperarte/view/Login.java
+++ app/src/main/java/com/example/recuperarte/view/Login.java
@@ -4,13 +4,28 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import android.content.Intent;
+
+import android.view.View;
+
+import android.widget.TextView;
+
 import com.example.recuperarte.R;
 
 public class Login extends AppCompatActivity {
+
+    private TextView tvSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        tvSignUp = findViewById(R.id.textSignUP);
+        tvSignUp.setOnClickListener(v -> {
+            Intent intent = new Intent(Login.this, SignUp.class);
+            startActivity(intent);
+            finish();
+        });
     }
 }
--- app/src/main/java/com/example/recuperarte/view/SignUp.java
+++ app/src/main/java/com/example/recuperarte/view/SignUp.java
@@ -1,16 +1,26 @@
 package com.example.recuperarte.view;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.recuperarte.R;
 
 public class SignUp extends AppCompatActivity {
 
+    private TextView tvLogin;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
+        tvLogin = findViewById(R.id.textLogin);
+        tvLogin.setOnClickListener(v -> {
+            Intent intent = new Intent(SignUp.this, Login.class);
+            startActivity(intent);
+            finish();
+        });
     }
 }
