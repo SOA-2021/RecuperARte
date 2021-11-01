fc1ad29fb46aded8510c44e5a5ae62e6d94fd33c
NewtonGauss <germanmerkel9@gmail.com>
2021-11-01 17:32:57 -0300

    Registro

---
+++ app/src/main/java/com/grupo15/recuperarte/view/MainActivity.java
@@ -0,0 +1,16 @@
+package com.grupo15.recuperarte.view;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.grupo15.recuperarte.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
--- app/src/main/java/com/grupo15/recuperarte/view/SignUp.java
+++ app/src/main/java/com/grupo15/recuperarte/view/SignUp.java
@@ -2,14 +2,27 @@ package com.grupo15.recuperarte.view;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
 import android.view.Window;
 import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.os.HandlerCompat;
 
 import com.grupo15.recuperarte.R;
+import com.grupo15.recuperarte.api_catedra.api.ApiException;
+import com.grupo15.recuperarte.api_catedra.api.IApiClient;
+import com.grupo15.recuperarte.api_catedra.model.User;
+import com.grupo15.recuperarte.global.Conf;
 
 public class SignUp extends AppCompatActivity {
+    Handler handler = HandlerCompat.createAsync(Looper.getMainLooper());
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -24,5 +37,53 @@ public class SignUp extends AppCompatActivity {
             startActivity(intent);
             finish();
         });
+        Button signup = findViewById(R.id.signup_button);
+        signup.setOnClickListener(b -> doSignUp((Button)b));
+    }
+
+    private void doSignUp(Button b) {
+        b.setActivated(false);
+
+        EditText nameInput = findViewById(R.id.name_input);
+        EditText lastnameInput = findViewById(R.id.lastname_input);
+        EditText emailInput = findViewById(R.id.email_input);
+        EditText passwordInput = findViewById(R.id.password_input);
+
+        String name = nameInput.getText().toString();
+        String lastname = lastnameInput.getText().toString();
+        String email = emailInput.getText().toString();
+        String password = passwordInput.getText().toString();
+
+        new Thread(()-> {
+            Conf c = Conf.getInstance();
+            IApiClient api = c.apiClient();
+            User newUser = new User(name, lastname, 123, email);
+            try {
+                api.registerUser(newUser, password);
+                handler.post(this::onSuccess);
+            } catch ( ApiException e ) {
+                handler.post(() -> onError(b, String.format("Error: %s", e.getMessage())));
+            }
+        }).start();
+    }
+
+    private void onSuccess() {
+        Toast.makeText(
+                getBaseContext(),
+                "Registro correcto",
+                Toast.LENGTH_SHORT
+        ).show();
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private void onError(Button b, String errMsg) {
+        b.setActivated(true);
+        Toast.makeText(
+                getBaseContext(),
+                errMsg,
+                Toast.LENGTH_SHORT
+        ).show();
     }
 }
