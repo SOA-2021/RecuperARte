afc387d7af3a855fcd5c268c0873b82367928227
Leonel San Martin <31077603+leo9210@users.noreply.github.com>
2021-11-02 10:03:45 -0300

    Se actualizan campos de la pantalla de registro

--- app/src/main/java/com/grupo15/recuperarte/view/Login.java
+++ app/src/main/java/com/grupo15/recuperarte/view/Login.java
@@ -1,11 +1,14 @@
 package com.grupo15.recuperarte.view;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.os.HandlerCompat;
 
 import android.os.Bundle;
 
 import android.content.Intent;
 
+import android.os.Handler;
+import android.os.Looper;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
@@ -22,7 +25,7 @@ import com.grupo15.recuperarte.global.Conf;
 
 public class Login extends AppCompatActivity {
 
-    Button btnLogin;
+    Handler handler = HandlerCompat.createAsync(Looper.getMainLooper());
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,38 +34,14 @@ public class Login extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_login);
-
         TextView tvSignUp = findViewById(R.id.textSignUP);
         tvSignUp.setOnClickListener(v -> {
             Intent intent = new Intent(Login.this, SignUp.class);
             startActivity(intent);
             finish();
         });
-
-        EditText emailField = findViewById(R.id.emailField);
-        EditText passwordField = findViewById(R.id.passwordField);
-
-        btnLogin = findViewById(R.id.btnLogin);
-        btnLogin.setOnClickListener(v -> {
-            String email = emailField.getText().toString();
-            String password = passwordField.getText().toString();
-            IApiClient api = Conf.getInstance().apiClient();
-            try {
-                api.login(email,password);
-            } catch (ApiException e) {
-                e.printStackTrace();
-                Toast.makeText(this,"Usuario incorrecto",Toast.LENGTH_LONG).show();
-            }
-
-            //IntentIntegrator integrator = new IntentIntegrator(Login.this);
-            //integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE);
-            //integrator.setPrompt("LECTOR DE CODIGO QR");
-            //integrator.setCameraId(0);
-            //integrator.setBeepEnabled(true);
-            //integrator.setBarcodeImageEnabled(true);
-            //integrator.initiateScan();
-        });
-
+        Button btnLogin = findViewById(R.id.btnLogin);
+        btnLogin.setOnClickListener(b -> doLogin((Button)b));
     }
 
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -72,10 +51,60 @@ public class Login extends AppCompatActivity {
             if (result.getContents() == null) {
                 Toast.makeText(this, "Lectura cancelada", Toast.LENGTH_LONG).show();
             } else {
-                Toast.makeText(this, result.getContents(), Toast.LENGTH_LONG).show();
+                if (result.getContents().equals("123456789"))
+                    handler.post(this::onSuccess);
+                else
+                    Toast.makeText(this, "Codigo Incorrecto", Toast.LENGTH_LONG).show();
             }
         } else {
             super.onActivityResult(requestCode, resultCode, data);
         }
+    }
+
+    private void doLogin(Button b) {
+        b.setActivated(false);
+
+        EditText emailField = findViewById(R.id.emailField);
+        EditText passwordField = findViewById(R.id.passwordField);
+
+        String email = emailField.getText().toString();
+        String password = passwordField.getText().toString();
+
+        new Thread(()-> {
+            Conf c = Conf.getInstance();
+            IApiClient api = c.apiClient();
+            try {
+                api.login(email, password);
+                IntentIntegrator integrator = new IntentIntegrator(Login.this);
+                integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE);
+                integrator.setPrompt("LECTOR DE CODIGO QR");
+                integrator.setCameraId(0);
+                integrator.setBeepEnabled(true);
+                integrator.setBarcodeImageEnabled(true);
+                integrator.initiateScan();
+            } catch ( ApiException e ) {
+                handler.post(() -> onError(b, String.format("Error: %s", e.getMessage())));
+            }
+        }).start();
+    }
+
+    private void onSuccess() {
+        Toast.makeText(
+                getBaseContext(),
+                "Login correcto",
+                Toast.LENGTH_SHORT
+        ).show();
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    private void onError(Button b, String errMsg) {
+        b.setActivated(true);
+        Toast.makeText(
+                getBaseContext(),
+                errMsg,
+                Toast.LENGTH_SHORT
+        ).show();
     }
 }
